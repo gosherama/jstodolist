1f5534c81e6069dd9d9fa5c7e96879a80d227607
gosher <macciak@gmail.com>
2017-04-03 20:15:40 +0000

    v6

--- todo.js
+++ todo.js
@@ -1,8 +1,18 @@
 var todolist = {
     todos: [],
     displayTodos: function(){
-    console.log('My todos:', this.todos);
-        },
+        if (this.todos.length === 0) {
+            console.log('You have no todos !!');
+        } else {
+            for(var i = 0; i < this.todos.length; i++) {
+                if(this.todos[i].completed === true) {
+                    console.log('(x)',this.todos[i].todoText);
+                } else {
+                    console.log('( )',this.todos[i].todoText);
+                }
+            }
+        }
+    },
     addTodo: function(todoText){
         this.todos.push({
             todoText:todoText,
@@ -21,6 +31,30 @@ var todolist = {
     toggleCompleted: function(position) {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    toggleAll: function() {
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        
+        //Get number of completed todos
+        for(var i = 0; i < totalTodos; i++) {
+            if(this.todos[i].completed === true){
+            completedTodos++;
+            }
+        }
+        
+        //Case 1: if all true, make everything false
+        if (completedTodos === totalTodos) {
+            for(var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            }
+        //Case 2: otherwise, make everything true
+        } else {
+            for(var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = true;
+            }
+        }
         this.displayTodos();
     }
     };
